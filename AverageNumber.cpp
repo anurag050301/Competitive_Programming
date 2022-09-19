4b28bb989a33094e236885cb4c1325b14cc43748
anurag050301 <84004486+anurag050301@users.noreply.github.com>
2022-09-19 23:33:55 +0530

    Add files via upload

---
+++ AverageNumber.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k, v, sum = 0, ans;
+        cin >> n >> k >> v;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+        if ((v * (k + n) - sum) % k != 0)
+            cout << -1 << endl;
+        else
+        {
+            ans = (v * (k + n) - sum) / k;
+            if (ans <= 0)
+                ans = -1;
+            cout << ans << endl;
+        }
+    }
+    return 0;
+}
