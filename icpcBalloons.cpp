b6b5499014103c5e7dcb2add88aa3b241b9dd50d
anurag050301 <84004486+anurag050301@users.noreply.github.com>
2022-09-20 23:47:46 +0530

    Add files via upload

---
+++ icpcBalloons.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> ar(n);
+        for (int i = 0; i < n; i++)
+            cin >> ar[i];
+        int count = 0, ans = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (ar[i] <= 7)
+                count++;
+            if (count == 7)
+            {
+                ans = i + 1;
+                count++;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
