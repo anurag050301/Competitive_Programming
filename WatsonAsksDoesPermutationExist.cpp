af658891826fce784953914a4af48cd233081274
anurag050301 <84004486+anurag050301@users.noreply.github.com>
2022-09-21 22:42:36 +0530

    Add files via upload

---
+++ WatsonAsksDoesPermutationExist.cpp
@@ -0,0 +1,39 @@
+//Watson asks Does Permutation Exist CodeChef, Problem Code:PERMEXIS
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <deque>
+#include <utility>
+#define ll long long int
+using namespace std;
+void solve()
+{
+    int n;
+    bool x = true;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        if ((i > 0) && (abs(v[i] - v[i - 1]) > 1))
+            x = false;
+    }
+    if (x == true)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
