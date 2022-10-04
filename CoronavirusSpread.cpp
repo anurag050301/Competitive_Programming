ef4b714adb12f9fa394ed65a10155bb63a7f8d0d
anurag050301 <84004486+anurag050301@users.noreply.github.com>
2022-10-04 12:32:16 +0530

    Add files via upload

---
+++ CoronavirusSpread.cpp
@@ -0,0 +1,45 @@
+// problem: Coronavirus Spread, code: MAY20
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <deque>
+#include <utility>
+#define ll long long int
+using namespace std;
+void solve()
+{
+    int n, count = 1;
+    cin >> n;
+    vector<int> a(n), b;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 1; i < n; i++)
+    {
+        if (abs(a[i] - a[i - 1]) <= 2)
+        {
+            count++;
+        }
+        else if (abs(a[i] - a[i - 1]) > 2)
+        {
+            b.push_back(count);
+            count = 1;
+        }
+    }
+    b.push_back(count);
+    sort(b.begin(), b.end());
+    cout << b[0] << " " << b[b.size() - 1] << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
