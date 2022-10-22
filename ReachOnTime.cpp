6629027ff12f8ff1e112df7d872d43d5a2261342
anurag050301 <anuragdubey05march@gmail.com>
2022-10-22 23:42:30 +0530

    Add files via upload

---
+++ ReachOnTime.cpp
@@ -0,0 +1,31 @@
+//Problem: Reach On time (CodeChef), Code:TIMELY
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <deque>
+#include <utility>
+#define ll long long int
+using namespace std;
+void solve()
+{
+    int x;
+    cin>>x;
+    if(x>=30)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
