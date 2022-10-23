aaa40fec709bdd2e91c581e9960c6e5b4a33b461
anurag050301 <anuragdubey05march@gmail.com>
2022-10-23 12:47:12 +0530

    Add files via upload

---
+++ HoopJump.cpp
@@ -0,0 +1,28 @@
+//Problem: Hoop Jump (CodeChef), Code: HOOPS
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <deque>
+#include <utility>
+#define ll long long int
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<abs(n/2)+1<<endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
