229ef83f1b45f398d77db2d6fb31bb88a342857c
anurag050301 <anuragdubey05march@gmail.com>
2022-11-02 23:20:08 +0530

    Add files via upload

---
+++ AvoidSquaresPlease.cpp
@@ -0,0 +1,30 @@
+//Problem: Avoid Square Please (CodeChef), Code: AVDSQUAREPLZ
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <deque>
+#include <utility>
+#define ll long long int
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cout<<i<<" ";
+    cout<<endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
